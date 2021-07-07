84539f9c591f4202d55809dfff68f0655c210641
BaraniAswini <baranirock8@gmail.com>
2021-07-07 13:27:37 +0530

    Ravi commit

---
+++ src/org/test/GitHub.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class GitHub {
+  
+	public static void password() {
+		System.out.println("Password");
+	}
+	public static void main(String[] args) {
+		password();
+	}
+	
+}
